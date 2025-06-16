00cfd7bf79d248892201e41fd2d6d25186963780
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-06-16 08:49:33 +0500

    Fatching data from stripe

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
 import { Button } from "@/components/ui/button";
-import{ stripe} from "@/lib/strip";
+import { stripe } from "@/lib/strip";
 
 const Home = async () => {
   const products = await stripe.products.list({
-    expand: 
-  })
+    expand: ["data.default_price"],
+    limit: 5,
+  });
+  console.log(products);
   return (
     <div>
-      <h1>Home</h1>
-      <Button>Click me</Button>
+      <section>
+        <div className="">
+          <div className="">
+            <h2>Wecome to My Ecommrece</h2>
+            <p>Discover the latest product at the best prices</p>
+            <Button asChild variant="default">
+              Browse All Products
+            </Button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
