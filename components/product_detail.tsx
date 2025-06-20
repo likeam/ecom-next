610778f1df11916e8e1b0052c2e70f52e451c145
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-06-20 08:53:42 +0500

    Building Cart

--- components/product_detail.tsx
+++ components/product_detail.tsx
@@ -1,12 +1,65 @@
+"use client";
+import { useCartStore } from "@/store/cart-store";
+import Image from "next/image";
 import React from "react";
 import Stripe from "stripe";
+import { Button } from "./ui/button";
 
 interface Props {
   product: Stripe.Product;
 }
 
 const ProductDetail = ({ product }: Props) => {
-  return <div>ProductDetail</div>;
+  const { items, addItem, removeItem } = useCartStore();
+  const price = product.default_price as Stripe.Price;
+  const cartItem = items.find((item) => item.id === product.id);
+  const quantity = cartItem ? cartItem.quantity : 0;
+
+  const onAddItem = () => {
+    addItem({
+      id: product.id,
+      name: product.name,
+      price: price.unit_amount as number,
+      imageUrl: product.images ? product.images[0] : null,
+      quantity: 1,
+    });
+  };
+
+  return (
+    <div>
+      {product.images && product.images[0] && (
+        <div className="relative h-96 w-full md:w-1/2 rounded-lg overflow-hidden">
+          <Image
+            src={product.images[0]}
+            alt={product.name}
+            height={140}
+            width={140}
+            layout="full"
+            objectFit="cover"
+            className=" group-hover:opacity-90 transition-opacity duration-300 rounded-t-lg"
+          />
+        </div>
+      )}
+      <div className="md:w-1/2">
+        <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
+        {product.description && (
+          <p className="text-gray-700 mb-4">{product.description}</p>
+        )}
+        {price && price.unit_amount && (
+          <p className="text-lg font-semibold text-gray-900">
+            Rs{(price.unit_amount / 100).toFixed(0)}
+          </p>
+        )}
+        <div className="flex items-center space-x-4">
+          <Button variant="outline" onClick={() => removeItem(product.id)}>
+            –
+          </Button>
+          <span className="text-lg font-semibold">{quantity}</span>
+          <Button onClick={onAddItem}>+</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetail;
