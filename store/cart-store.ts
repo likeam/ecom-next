08f5abaa82a776e6437f405fb18495b9fd399597
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-06-20 14:05:07 +0500

    Removing error

--- store/cart-store.ts
+++ store/cart-store.ts
@@ -4,7 +4,7 @@ import { persist } from "zustand/middleware";
 export interface CartItem {
   id: string;
   name: string;
-  price: string;
+  price: number;
   imageUrl: string | null;
   quantity: number;
 }
